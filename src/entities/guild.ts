bbb983250f6edfc6aa53a81eb75873d39ded9a7a
Elias Kleppinger <eliaskleppinger@protonmail.com>
2021-10-28 21:00:26 +0200

    ✅ Finished translation system

--- src/entities/guild.ts
+++ src/entities/guild.ts
@@ -1,7 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, PrimaryColumn } from "typeorm";
+import { t as translate } from "../helpers/language.js";
 
 @Entity()
 export default class Guild extends BaseEntity {
 	@PrimaryColumn()
 	id: string;
+
+	@Column()
+	prefix: string;
+
+	@Column()
+	language: string;
+
+	t(node: string, ...args: string[]) {
+		return translate(this.language, node, ...args);
+	}
 }
