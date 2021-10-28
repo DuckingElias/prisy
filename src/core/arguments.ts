0c963b3ce0ae7e53bf9e92a7b627b5ff588479b2
Elias Kleppinger <eliaskleppinger@protonmail.com>
2021-10-28 22:15:06 +0200

    ✅ Command system done

--- src/core/arguments.ts
+++ src/core/arguments.ts
@@ -1,16 +1,35 @@
 import { GuildMember } from "discord.js";
 
 export default class Arguments {
+	public readonly argumentValues: { [key: string]: any };
+
+	constructor(argumentValues: { [key: string]: any }) {
+		this.argumentValues = argumentValues;
+	}
+
 	string(name: string): string {
-		return "";
+		return this.argumentValues[name];
+	}
+
+	text(name: string): string {
+		if (this.argumentValues[name].startsWith('"') && this.argumentValues[name].endsWith('"')) {
+			return this.argumentValues[name].substring(1, this.argumentValues[name].length - 1);
+		}
+		return this.string(name);
 	}
 
 	number(name: string): number {
-		return 1;
+		try {
+			return Number(this.argumentValues[name]);
+		} catch (e) {
+			return null;
+		}
 	}
 
 	boolean(name: string): boolean {
-		return true;
+		if (this.argumentValues[name] === "true") return true;
+		if (this.argumentValues[name] === "false") return false;
+		return null;
 	}
 
 	user(name: string): GuildMember {
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,9 +1,11 @@
-import { GuildChannel, Message } from "discord.js";
+import { GuildChannel, Message, MessageEmbed } from "discord.js";
 import { getConnection } from "typeorm";
+import Arguments from "../core/arguments.js";
 import Event from "../core/event.js";
 import Guild from "../entities/guild.js";
 import { getCommands } from "../helpers/command.js";
 import { getGuild } from "../helpers/database.js";
+import { ArgumentType } from "../interfaces/argument.js";
 
 export default class MessageCreateEvent extends Event<"messageCreate"> {
 	constructor() {
@@ -23,10 +25,117 @@ export default class MessageCreateEvent extends Event<"messageCreate"> {
 		text = text.substring(prefix.length);
 		const splitted = text.split(" ");
 		const commandName = splitted[0].trim();
+		splitted.shift();
 		const command = getCommands().find((c) => {
 			if (c.name == commandName || c.aliases.includes(commandName)) return true;
 			return false;
 		});
-		if (command) command.execute(guild, message, null);
+		let argsRaw = splitted.join(" ");
+		let args = argsRaw.split(/\s(?=(?:(?:[^"]*"){2})*[^"]*$)/).map((arg) => arg.trim());
+		if (args[0].trim() == "") args.shift();
+
+		function isArgumentTypeValid(original: ArgumentType, check: ArgumentType): boolean {
+			if (original == ArgumentType.BOOLEAN) {
+				if (check == ArgumentType.BOOLEAN) return true;
+			}
+
+			if (original == ArgumentType.NUMBER) {
+				if (check == ArgumentType.NUMBER) return true;
+			}
+
+			if (original == ArgumentType.TEXT) {
+				if (check == ArgumentType.NUMBER) return true;
+				if (check == ArgumentType.TEXT) return true;
+				if (check == ArgumentType.BOOLEAN) return true;
+				if (check == ArgumentType.STRING) return true;
+				if (check == ArgumentType.USER) return true;
+			}
+
+			if (original == ArgumentType.STRING) {
+				if (check == ArgumentType.NUMBER) return true;
+				if (check == ArgumentType.BOOLEAN) return true;
+				if (check == ArgumentType.STRING) return true;
+				if (check == ArgumentType.USER) return true;
+			}
+
+			if (original == ArgumentType.USER) {
+				if (check == ArgumentType.USER) return true;
+			}
+
+			return false;
+		}
+
+		function getArgumentType(value: string): ArgumentType {
+			if (value.startsWith('"') && value.endsWith('"')) {
+				value = value.substring(1, value.length - 1);
+				value = value.trim();
+			}
+			if (value == "true" || value == "false") return ArgumentType.BOOLEAN;
+			if (value.startsWith("<@") && value.endsWith(">")) return ArgumentType.USER;
+			if (!isNaN(value as unknown as number)) return ArgumentType.NUMBER;
+			if (value.includes(" ")) return ArgumentType.TEXT;
+			return ArgumentType.STRING;
+		}
+
+		const argumentDefinitions = command.arguments();
+		const argumentValues = {};
+		for (let i = 0; i < argumentDefinitions.length; i++) {
+			const argumentDefinition = argumentDefinitions[i];
+			const arg = args[i];
+
+			if (arg == undefined && !argumentDefinition.optional) {
+				await message.reply({
+					embeds: [
+						new MessageEmbed()
+							.setColor(0xff0000)
+							.setTitle(
+								guild.t(
+									"commands.error_messages.error_title",
+									guild.t("commands." + command.name + ".title"),
+								),
+							)
+							.setAuthor(message.author.username, message.author.avatarURL())
+							.setDescription(
+								guild.t(
+									"commands.error_messages.missing_argument",
+									guild.t(
+										"commands." + command.name + ".args." + argumentDefinition.name + ".title",
+									),
+								),
+							),
+					],
+				});
+				return;
+			}
+
+			if (!isArgumentTypeValid(argumentDefinition.type, getArgumentType(arg))) {
+				await message.reply({
+					embeds: [
+						new MessageEmbed()
+							.setColor(0xff0000)
+							.setTitle(
+								guild.t(
+									"commands.error_messages.error_title",
+									guild.t("commands." + command.name + ".title"),
+								),
+							)
+							.setAuthor(message.author.username, message.author.avatarURL())
+							.setDescription(
+								guild.t(
+									"commands.error_messages.wrong_argument",
+									guild.t(
+										"commands." + command.name + ".args." + argumentDefinition.name + ".title",
+									),
+								),
+							),
+					],
+				});
+				return;
+			}
+
+			argumentValues[argumentDefinition.name] = arg;
+		}
+
+		if (command) command.execute(guild, message, new Arguments(argumentValues));
 	}
 }
